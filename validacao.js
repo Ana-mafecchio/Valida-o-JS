e72d8ecd8d2a8f369d1f74d2ff2c19cf6c9003e0
ana.mf <froggitfroggers@gmail.com>
2026-05-29 22:22:27 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -17,4 +17,20 @@ function fnValidacao (){
 
     const campoAnoProducao = document.getElementById("anoProducao")
    
+    const regexAno = /^\d{4}$/;
+
+    //
+    if(!regexAno.test(campoAnoProducao)) {
+       return console.log('Coloque um ano com 4 digitos')
+    }
+
+    const anoNum = parseInt(campoAnoProducao, 10);
+    const anoAtual = new Date().getFullYear();
+
+    if (anoNum < 1888 || anoNum > anoAtual) {
+        return console.log('O ano deve estar entre 1888 e ' + anoAtual);
+        return false;
+    }
+     console.log('Ano válido!');
+    return true;
 }
